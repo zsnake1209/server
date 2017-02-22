d5abf8b020b49061e2c9afa015ffcba4548bb430
Vladislav Vaintroub <wlad@mariadb.com>
2017-02-22 19:13:06 +0000

    Prepare server code for MariaDB Backup
    - Backup should be able to load encryption plugins
    outside of mysqld. Thus, do not force loading MyISAM plugin
    in plugin_load.
    
    - Be prepared for current_thd == NULL in thd_increment_bytes_received
    - Add nt_service.cc to the file list for sql library on Windows,
    to avoid unresolved symbols.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -3105,7 +3105,7 @@ LONG WINAPI my_unhandler_exception_filter(EXCEPTION_POINTERS *ex_pointers)
 }
 
 
-static void init_signals(void)
+void init_signals(void)
 {
   if(opt_console)
     SetConsoleCtrlHandler(console_event_handler,TRUE);
@@ -3236,7 +3236,7 @@ static size_t my_setstacksize(pthread_attr_t *attr, size_t stacksize)
 
 #ifndef EMBEDDED_LIBRARY
 
-static void init_signals(void)
+void init_signals(void)
 {
   sigset_t set;
   struct sigaction sa;
